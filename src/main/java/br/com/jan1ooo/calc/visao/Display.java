5c78d3a930d350d41343310329f01714329983ab
Janio Filho <filhojanio67@gmail.com>
2022-11-28 21:06:44 -0300

    Display

--- src/main/java/br/com/jan1ooo/calc/visao/Display.java
+++ src/main/java/br/com/jan1ooo/calc/visao/Display.java
@@ -4,7 +4,16 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Display extends JPanel {
+
+    private final JLabel label;
+
     public Display(){
-        setBackground(Color.GREEN);
+        setBackground(new Color(49, 49, 50));
+        label = new JLabel("123913");
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("courier", Font.PLAIN, 30));
+
+        setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 20));
+        add(label);
     }
 }
