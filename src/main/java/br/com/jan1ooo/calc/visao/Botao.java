adb317982f5fe5895654a577f1640a969706ffb3
Janio Filho <filhojanio67@gmail.com>
2022-11-29 23:36:08 -0300

    Botao

--- src/main/java/br/com/jan1ooo/calc/visao/Botao.java
+++ src/main/java/br/com/jan1ooo/calc/visao/Botao.java
@@ -1,6 +1,16 @@
 package br.com.jan1ooo.calc.visao;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Botao extends JButton {
+
+    public Botao(String texto, Color cor){
+        setText(texto);
+        setOpaque(true);
+        setBackground(cor);
+        setForeground(Color.WHITE);
+        setFont(new Font("courier", Font.PLAIN, 15));
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    }
 }
--- src/main/java/br/com/jan1ooo/calc/visao/Teclado.java
+++ src/main/java/br/com/jan1ooo/calc/visao/Teclado.java
@@ -4,7 +4,12 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Teclado extends JPanel {
+
+    private final Color COR_CINZA_ESCURO = new Color(68, 68, 68);
+    private final Color COR_CINZA_CLARO = new Color(99, 99, 99);
+    private final Color COR_LARANJA = new Color(242, 163, 60);
+
     public Teclado(){
-        setBackground(Color.RED);
+
     }
 }
