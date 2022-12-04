a4d82ccfad4c5cc36ede785efb582e49c16e06d7
Janio Filho <filhojanio67@gmail.com>
2022-12-04 18:42:09 -0300

    Finalização

--- src/main/java/br/com/jan1ooo/calc/modelo/Memoria.java
+++ src/main/java/br/com/jan1ooo/calc/modelo/Memoria.java
@@ -6,14 +6,17 @@ import java.util.List;
 public class Memoria {
 
     private enum TipoComando {
-            ZERAR, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA
+            ZERAR, SINAL, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA
     }
 
     private static final Memoria instancia = new Memoria();
 
     private final List<MemoriaObservador> observadores = new ArrayList<>();
 
+    private TipoComando ultimaOperacao = null;
+    private boolean substituir = false;
     private String textoAtual =  "";
+    private String textoBuffer = "";
 
     private Memoria(){
 
@@ -35,13 +38,53 @@ public class Memoria {
 
         TipoComando tipoComando = detectarTipoComando(texto);
 
-        if("AC".equals(texto)){
-            textoAtual =  "";
-        }else{
-            textoAtual += texto;
+        if(tipoComando == null){
+            return;
+        }else if(tipoComando == TipoComando.ZERAR){
+            textoAtual = "";
+            textoBuffer = "";
+            substituir = false;
+            ultimaOperacao = null;
+        }else if(tipoComando == TipoComando.SINAL && textoAtual.contains("-")){
+            textoAtual = textoAtual.substring(1);
+        }else if(tipoComando == TipoComando.SINAL && !textoAtual.contains("-")){
+            textoAtual = "-" + textoAtual;
+        }else if(tipoComando == TipoComando.NUMERO || tipoComando == TipoComando.VIRGULA){
+            textoAtual = substituir ? texto : textoAtual + texto;
+            substituir = false;
+        } else {
+            substituir = true;
+            textoAtual = obterResultadoOperação();
+            textoBuffer = textoAtual;
+            ultimaOperacao = tipoComando;
         }
 
         observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+    }
+
+    private String obterResultadoOperação() {
+        if(ultimaOperacao == null || ultimaOperacao == TipoComando.IGUAL){
+            return textoAtual;
+        }
+
+        double numeroBuffer = Double.parseDouble(textoBuffer.replace(",", "."));
+        double numeroAtual = Double.parseDouble(textoAtual.replace(",", "."));
+
+        double resultado = 0;
+
+        if(ultimaOperacao == TipoComando.SOMA){
+            resultado = numeroBuffer + numeroAtual;
+        } else if (ultimaOperacao == TipoComando.SUB) {
+            resultado = numeroBuffer - numeroAtual;
+        }else if(ultimaOperacao == TipoComando.MULT){
+            resultado = numeroBuffer * numeroAtual;
+        }else if(ultimaOperacao == TipoComando.DIV){
+            resultado = numeroBuffer / numeroAtual;
+        }
+
+        String resultadoString = Double.toString(resultado).replace(".", ",");
+        boolean inteiro = resultadoString.endsWith(",0");
+        return inteiro ? resultadoString.replace(",0", "") : resultadoString;
     }
 
     private TipoComando detectarTipoComando(String texto) {
@@ -64,12 +107,13 @@ public class Memoria {
             }else if("*".equals(texto)){
                 return TipoComando.MULT;
             }else if("=".equals(texto)){
-                return TipoComando.IGUAL;
-            }else if(",".equals(texto)){
+
+            }else if("±".equals(texto)){
+                return TipoComando.SINAL;
+            }else if(",".equals(texto) && !textoAtual.contains(",")){
                 return TipoComando.VIRGULA;
             }
         }
-
         return null;
     }
 
